9e648b6e1ec165bc18b49040960c37085aafc692
Berk Tenkoğlu <berk.tenkoglu@gmail.com>
2024-02-23 17:05:33 +0300

    sprite animation update

--- script.js
+++ script.js
@@ -50,13 +50,17 @@ window.addEventListener('load',function(){
     class Player {
         constructor(game){
             this.game = game;
-            this.width= 50;
-            this.height= 100;
+            this.width= 120;
+            this.height= 190;
             this.x= 20;
             this.y= 100;
+            this.frameX = 0;
+            this.frameY = 0;
+            this.maxFrame = 37;
             this.speedY= 0;
             this.maxSpeed = 2;
             this.projectiles = [];
+            this.image = document.getElementById("player");
         }
         update(){
             if(this.game.keys.includes('w')) this.speedY = -this.maxSpeed;
@@ -68,10 +72,16 @@ window.addEventListener('load',function(){
                 projectile.update();
             });
             this.projectiles = this.projectiles.filter(projectile => !projectile.markedForDeletion);
+            if(this.frameX < this.maxFrame){
+                this.frameX++;
+            } else{
+                this.frameX = 0;
+            }
         }
         draw(context){
             context.fillStyle = 'black';
             context.fillRect(this.x, this.y, this.width,this.height);
+            context.drawImage(this.image, this.frameX * this.width, this.frameY * this.height,this.width,this.height, this.x, this.y, this.width, this.height);
             this.projectiles.forEach(projectile => {
                 projectile.draw(context);
             });
