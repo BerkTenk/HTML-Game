cc162476f32bc38a735fc74d080801ec610e5890
Berk Tenkoğlu <berk.tenkoglu@gmail.com>
2024-02-23 16:43:37 +0300

    layers updated

--- script.js
+++ script.js
@@ -128,15 +128,22 @@ window.addEventListener('load',function(){
             else this.x -= this.game.speed * this.speedModifier;
         }
         draw(context){
-           context.drawImage(this.image, this.x, this.y); 
+           context.drawImage(this.image, this.x, this.y);
+           context.drawImage(this.image, this.x+ this.width, this.y);
         }
     }
     class Background {
         constructor(game){
             this.game = game;
             this.image1 = document.getElementById("layer1");
-            this.layer1 = new Layer(this.game, this.image1, 1);
-            this.layers = [this.layer1];
+            this.image2 = document.getElementById("layer2");
+            this.image3 = document.getElementById("layer3");
+            this.image4 = document.getElementById("layer4");
+            this.layer1 = new Layer(this.game, this.image1, 0.2);
+            this.layer2 = new Layer(this.game, this.image2, 0.4);
+            this.layer3 = new Layer(this.game, this.image3, 1);
+            this.layer4 = new Layer(this.game, this.image4, 1.3);
+            this.layers = [this.layer1, this.layer2, this.layer3];
         }
         update(){
             this.layers.forEach(layer => layer.update());
@@ -218,6 +225,7 @@ window.addEventListener('load',function(){
             if(!this.gameOver) this.gameTime += deltaTime;
             if(this.gameTime > this.timeLimit) this.gameOver = true;
             this.background.update();
+            this.background.layer4.update();
             this.player.update();
             if(this.ammoTimer > this.ammoInterval){
                 if(this.ammo < this.maxAmmo){
@@ -259,7 +267,8 @@ window.addEventListener('load',function(){
             this.ui.draw(context);
             this.enemies.forEach(enemy => {
                 enemy.draw(context);
-            })
+            });
+            this.background.layer4.draw(context);
         }
         addEnemy() {
             this.enemies.push(new Angler1(this));
